ed656b94300e5579e04740195d9b4a79e5b7aa4d
alpenovDcode <alpenovevgeniy05@gmail.com>
2023-12-20 21:30:45 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Альпеша on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
