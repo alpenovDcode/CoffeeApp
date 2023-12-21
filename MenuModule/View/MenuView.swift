2905a6e1dfeb5900bb4ecbeb968ab4ef9d46ae62
alpenovDcode <alpenovevgeniy05@gmail.com>
2023-12-22 00:55:37 +0300

    Created MenuView. Need to add plus/minus buttons for counter of cups

---
+++ MenuModule/View/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  CoffeeApp
+//
+//  Created by Альпеша on 21.12.2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
